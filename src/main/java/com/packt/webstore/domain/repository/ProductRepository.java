b2d20645a99ed02f92ea94bc7659fccbba5cf64e
mgpatel <Krishna@Krishnas-MacBook-Pro.local>
2015-12-12 11:16:52 +1100

    initial version

---
+++ src/main/java/com/packt/webstore/domain/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.packt.webstore.domain.repository;
+
+import com.packt.webstore.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by Mayank on 10/12/15.
+ */
+
+public interface ProductRepository {
+
+    List<Product> getAllProducts();
+}
+
